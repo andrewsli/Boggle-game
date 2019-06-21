0a3024e99a10ee12648463a49163a39424806157
Andrew Li <andrewli0725@gmail.com>
2019-06-21 15:03:15 -0700

    updates game based on server response

--- static/script.js
+++ static/script.js
@@ -1,24 +1,41 @@
 
-// handleFormSubmission and AJAX request to "/guess"
-// # - prevent form default
-// # - grab input
-// # - LATER TO DO: check if word has already been guessed
-// # - axios request to "/guess" using input as parameters
-// # - based on response (ok, not-on-board, not-a-word) render different template (TO DO: figure out that logic)
 
-
-
+/** Handle submission of word guess by:
+ * - Sending guessed word to server ("check-word") to validate 
+ * - Passing server response to updateGame
+ */
 async function handleFormSubmission(evt){
-  evt.preventDefault(); // prevent form refresh
+  evt.preventDefault(); 
   let guessedWord = $("#guess").val();
-  console.log("guessed word: ", guessedWord);
+  // console.log("guessed word: ", guessedWord);
 
   let params = {"guess" : guessedWord}
   let response = await axios.get("/check-word", {params})
-  console.log("JS handleFormSubmission: ", response.data) // {"result": something}
+  // console.log("JS handleFormSubmission: ", response.data) // {"result": something}
   $("#form").trigger("reset");
 
-  return response.data 
+  updateGame(response.data, guessedWord);
+
+  return
 }
 
 $("#form").on("submit", handleFormSubmission)
+
+/* Modifies HTML based on validity of guessed word (from server response) */
+function updateGame(responseObj, guessedWord){
+
+  wordStatus = responseObj.result;
+
+  if (wordStatus === "ok"){
+    $("#response-message").text("Nice find!");
+    $("#validated-words").append(`<li>${guessedWord}</li>`);
+
+  } else if (wordStatus === "not-word"){
+    $("#response-message").text("Not a valid word, try again");
+
+  } else if (wordStatus === "not-on-board"){
+    $("#response-message").text("That word's not on the board, try again");
+  }
+
+  return
+}
